5cc5ea31fe659bef2d353161f09fd37067aa534d
Lukasz Pater <Lukasz.Pater@cern.ch>
2019-06-15 23:47:31 +0200

    Add new properties to PartStock POJO

--- src/main/java/ch/cern/eam/wshub/core/services/material/entities/PartStock.java
+++ src/main/java/ch/cern/eam/wshub/core/services/material/entities/PartStock.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 public class PartStock implements Serializable {
 
 	private String storeCode;
+	private String storeDesc;
 	private String bin;
 	private String lot;
 	private String qtyOnHand;
 	private String partCode;
+	private String repairQuantity;
+	private String assetCode;
 	
 	public String getStoreCode() {
 		return storeCode;
@@ -16,30 +19,44 @@ public class PartStock implements Serializable {
 	public void setStoreCode(String storeCode) {
 		this.storeCode = storeCode;
 	}
+
 	public String getBin() {
 		return bin;
 	}
 	public void setBin(String bin) {
 		this.bin = bin;
 	}
+
 	public String getLot() {
 		return lot;
 	}
 	public void setLot(String lot) {
 		this.lot = lot;
 	}
+
 	public String getQtyOnHand() {
 		return qtyOnHand;
 	}
 	public void setQtyOnHand(String qtyOnHand) {
 		this.qtyOnHand = qtyOnHand;
 	}
+
 	public String getPartCode() {
 		return partCode;
 	}
 	public void setPartCode(String partCode) {
 		this.partCode = partCode;
 	}
+
+	public String getStoreDesc() { return storeDesc; }
+	public void setStoreDesc(String storeDesc) { this.storeDesc = storeDesc; }
+
+	public String getRepairQuantity() { return repairQuantity; }
+	public void setRepairQuantity(String repairQuantity) { this.repairQuantity = repairQuantity; }
+
+	public String getAssetCode() { return assetCode; }
+	public void setAssetCode(String assetCode) { this.assetCode = assetCode; }
+
 	@Override
 	public String toString() {
 		return "PartStock ["
