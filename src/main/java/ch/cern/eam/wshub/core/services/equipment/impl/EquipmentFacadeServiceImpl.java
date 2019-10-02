9f3f72b528c32df42702c0f7afb1ed549f56e4d5
Lukasz Pater <Lukasz.Pater@cern.ch>
2019-10-02 16:18:51 +0200

    Add more info for fetching eqp. types

--- src/main/java/ch/cern/eam/wshub/core/services/equipment/impl/EquipmentFacadeServiceImpl.java
+++ src/main/java/ch/cern/eam/wshub/core/services/equipment/impl/EquipmentFacadeServiceImpl.java
@@ -201,7 +201,7 @@ public class EquipmentFacadeServiceImpl implements EquipmentFacadeService {
         types.put("Material", "M");
         types.put("Location", "L");
         types.put("Localisation", "L");
-
+        //
         GridRequest gridRequest = new GridRequest("LVREPCOGALLEQUIPMENT", GridRequest.GRIDTYPE.LOV);
         gridRequest.getGridRequestFilters().add(new GridRequestFilter("code", equipmentCode, "="));
         GridRequestResult requestResult = gridsService.executeQuery(inforContext, gridRequest);
