3d3899affbaab92e10eecfcbd8ddd7a7b5947e84
Lukasz Pater <Lukasz.Pater@cern.ch>
2020-04-13 22:51:40 +0200

    Add Operation annotations to LocationService interface

--- src/main/java/ch/cern/eam/wshub/core/services/equipment/LocationService.java
+++ src/main/java/ch/cern/eam/wshub/core/services/equipment/LocationService.java
@@ -1,6 +1,8 @@
 package ch.cern.eam.wshub.core.services.equipment;
 
+import ch.cern.eam.wshub.core.annotations.Operation;
 import ch.cern.eam.wshub.core.client.InforContext;
+import ch.cern.eam.wshub.core.services.INFOR_OPERATION;
 import ch.cern.eam.wshub.core.services.entities.BatchResponse;
 import ch.cern.eam.wshub.core.services.equipment.entities.Location;
 import ch.cern.eam.wshub.core.tools.InforException;
@@ -8,13 +10,28 @@ import ch.cern.eam.wshub.core.tools.InforException;
 import java.util.List;
 
 public interface LocationService {
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_BC)
     BatchResponse<String> createLocationBatch(InforContext context, List<Location> locations) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_BR)
     BatchResponse<Location> readLocationBatch(InforContext context, List<String> locationCodes) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_BU)
     BatchResponse<String> updateLocationBatch(InforContext context, List<Location> locations) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_BD)
     BatchResponse<String> deleteLocationBatch(InforContext context, List<String> locationCodes) throws InforException;
 
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_R)
     Location readLocation(InforContext context, String locationCode) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_C)
     String createLocation(InforContext context, Location locationParam) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_U)
     String updateLocation(InforContext context, Location locationParam) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.LOCATION_D)
     String deleteLocation(InforContext context, String locationCode) throws InforException;
 }
