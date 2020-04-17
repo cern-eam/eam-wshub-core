3c67abac2d43bdebb17e4dbce09745e9130a2ff2
Lukasz Pater <Lukasz.Pater@cern.ch>
2020-04-17 16:57:50 +0200

    Support number of milliseconds in the DateAdapter

--- src/main/java/ch/cern/eam/wshub/core/adapters/DateAdapter.java
+++ src/main/java/ch/cern/eam/wshub/core/adapters/DateAdapter.java
@@ -10,47 +10,58 @@ public class DateAdapter extends XmlAdapter<String, Date> {
 
     @Override
     public String marshal(Date date) throws Exception {
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm", Locale.ENGLISH);
-		return formatter.format(cal.getTime()).toUpperCase();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy HH:mm", Locale.ENGLISH);
+        return formatter.format(cal.getTime()).toUpperCase();
     }
 
     @Override
     public Date unmarshal(String date) throws Exception {
-    	//
-    	if (date == null) {
-    		return null;
-    	}
-    	// 
-    	if (date.trim().equals("")) {
-    		return new Date(0L);
-    	}
-    	//
-		if (date.trim().equalsIgnoreCase("SYSDATE")) {
-			return Calendar.getInstance().getTime();
-		}
-		String[] formatStrings = {"dd-MMM-yyyy HH:mm", "dd-MMM-yyyy", "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd HH:mm", "yyyy-MM-dd", "dd/MM/yyyy HH:mm:ss","dd/MM/yyyy"};
+        //
+        if (date == null) {
+            return null;
+        }
+        //
+        if (date.trim().equals("")) {
+            return new Date(0L);
+        }
 
-		Exception exception = null;
-		for (String formatString : formatStrings)
-		{
-			try
-			{
-				SimpleDateFormat formatter = new SimpleDateFormat(formatString, Locale.ENGLISH);
-				Calendar calendar = Calendar.getInstance();
-				calendar.setTime(formatter.parse(date));
-				return calendar.getTime();
-			}
-			catch (Exception e) {
-				exception = e;
-			}
-			
-		}
-		
-		throw exception;
+        // Check if the passed string is the number of milliseconds from epoch
+        try {
+            return new Date(Long.parseLong(date));
+        } catch (NumberFormatException exception) {
+            // Nothing wrong here, the passes
+        }
+
+        //
+        if (date.trim().equalsIgnoreCase("SYSDATE")) {
+            return Calendar.getInstance().getTime();
+        }
+        String[] formatStrings = {"dd-MMM-yyyy HH:mm:ss",
+                "dd-MMM-yyyy HH:mm",
+                "dd-MMM-yyyy",
+                "yyyy-MM-dd HH:mm:ss",
+                "yyyy-MM-dd HH:mm",
+                "yyyy-MM-dd",
+                "dd/MM/yyyy HH:mm:ss",
+                "dd/MM/yyyy HH:mm",
+                "dd/MM/yyyy"};
+
+        Exception exception = null;
+        for (String formatString : formatStrings) {
+            try {
+                SimpleDateFormat formatter = new SimpleDateFormat(formatString, Locale.ENGLISH);
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(formatter.parse(date));
+                return calendar.getTime();
+            } catch (Exception e) {
+                exception = e;
+            }
+
+        }
+
+        throw exception;
     }
 
-
-    
 }
