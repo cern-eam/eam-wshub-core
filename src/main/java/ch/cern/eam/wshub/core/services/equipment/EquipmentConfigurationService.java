9753f795a7eeb0e9f138155aa832e45318510126
Ricardo Almeida <ricardo.almeida@cern.ch>
2021-06-14 17:32:45 +0200

    Equipment Configuration CRUD services

--- src/main/java/ch/cern/eam/wshub/core/services/equipment/EquipmentConfigurationService.java
+++ src/main/java/ch/cern/eam/wshub/core/services/equipment/EquipmentConfigurationService.java
@@ -1,21 +1,46 @@
 package ch.cern.eam.wshub.core.services.equipment;
 
+import ch.cern.eam.wshub.core.annotations.Operation;
 import ch.cern.eam.wshub.core.client.InforContext;
+import ch.cern.eam.wshub.core.interceptors.LogDataReferenceType;
+import ch.cern.eam.wshub.core.services.INFOR_OPERATION;
+import ch.cern.eam.wshub.core.services.entities.BatchResponse;
 import ch.cern.eam.wshub.core.services.equipment.entities.EquipmentConfigurationEntity;
+import ch.cern.eam.wshub.core.services.equipment.entities.EquipmentConfigurationId;
+import ch.cern.eam.wshub.core.services.workorders.entities.WorkOrder;
 import ch.cern.eam.wshub.core.tools.InforException;
+import net.datastream.schemas.mp_entities.equipmentconfigurationdefault_001.EquipmentConfigurationDefault;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface EquipmentConfigurationService {
 
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_C, logDataReference1 = LogDataReferenceType.RESULT)
     String createEquipmentConfiguration(InforContext context, EquipmentConfigurationEntity equipmentConfiguration) throws InforException;
 
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_R, logDataReference1 = LogDataReferenceType.INPUT)
+    EquipmentConfigurationEntity readEquipmentConfiguration(InforContext context, EquipmentConfigurationId equipmentConfigurationId) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_U, logDataReference1 = LogDataReferenceType.INPUT)
     String updateEquipmentConfiguration(InforContext context, EquipmentConfigurationEntity equipmentConfiguration) throws InforException;
 
-    EquipmentConfigurationEntity readEquipmentConfiguration(InforContext context, String equipmentConfigurationCode, BigDecimal revisionNum) throws InforException;
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_D, logDataReference1 = LogDataReferenceType.INPUT)
+    String deleteEquipmentConfiguration(InforContext context, EquipmentConfigurationId equipmentConfigurationId) throws InforException;
 
-    EquipmentConfigurationEntity readEquipmentDefaultConfiguration(InforContext context, String equipmentConfigurationCode) throws InforException;
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_RD, logDataReference1 = LogDataReferenceType.RESULT)
+    EquipmentConfigurationDefault readEquipmentDefaultConfiguration(InforContext context) throws InforException;
 
-    String deleteEquipmentConfiguration(InforContext context, String equipmentConfigurationCode) throws InforException;
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_BC, logDataReference1 = LogDataReferenceType.RESULT)
+    BatchResponse<String> createEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationEntity> equipmentConfigurationList) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_BR, logDataReference1 = LogDataReferenceType.INPUT)
+    BatchResponse<EquipmentConfigurationEntity> readEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationId> equipmentConfigurationIdList) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_BU, logDataReference1 = LogDataReferenceType.INPUT)
+    BatchResponse<String> updateEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationEntity> equipmentConfigurationList) throws InforException;
+
+    @Operation(logOperation = INFOR_OPERATION.EQP_CONF_BD, logDataReference1 = LogDataReferenceType.INPUT)
+    BatchResponse<String> deleteEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationId> equipmentConfigurationIdList) throws InforException;
 
 }
--- src/main/java/ch/cern/eam/wshub/core/services/equipment/impl/EquipmentConfigurationServiceImpl.java
+++ src/main/java/ch/cern/eam/wshub/core/services/equipment/impl/EquipmentConfigurationServiceImpl.java
@@ -1,20 +1,31 @@
 package ch.cern.eam.wshub.core.services.equipment.impl;
 
 import ch.cern.eam.wshub.core.client.InforContext;
+import ch.cern.eam.wshub.core.services.entities.BatchResponse;
 import ch.cern.eam.wshub.core.services.equipment.EquipmentConfigurationService;
-import ch.cern.eam.wshub.core.services.equipment.entities.Equipment;
 import ch.cern.eam.wshub.core.services.equipment.entities.EquipmentConfigurationEntity;
+import ch.cern.eam.wshub.core.services.equipment.entities.EquipmentConfigurationId;
 import ch.cern.eam.wshub.core.tools.ApplicationData;
+import ch.cern.eam.wshub.core.tools.DataTypeTools;
 import ch.cern.eam.wshub.core.tools.InforException;
 import ch.cern.eam.wshub.core.tools.Tools;
 import net.datastream.schemas.mp_entities.equipmentconfiguration_001.EquipmentConfiguration;
+import net.datastream.schemas.mp_entities.equipmentconfigurationdefault_001.EquipmentConfigurationDefault;
 import net.datastream.schemas.mp_fields.EQUIPMENTCONFIGURATIONID_Type;
+import net.datastream.schemas.mp_functions.mp3224_001.MP3224_GetEquipmentConfigurationDefault_001;
+import net.datastream.schemas.mp_functions.mp3225_001.MP3225_AddEquipmentConfiguration_001;
+import net.datastream.schemas.mp_functions.mp3226_001.MP3226_SyncEquipmentConfiguration_001;
+import net.datastream.schemas.mp_functions.mp3227_001.MP3227_DeleteEquipmentConfiguration_001;
 import net.datastream.schemas.mp_functions.mp3228_001.MP3228_GetEquipmentConfiguration_001;
+import net.datastream.schemas.mp_results.mp3224_001.MP3224_GetEquipmentConfigurationDefault_001_Result;
+import net.datastream.schemas.mp_results.mp3227_001.MP3227_DeleteEquipmentConfiguration_001_Result;
 import net.datastream.schemas.mp_results.mp3228_001.MP3228_GetEquipmentConfiguration_001_Result;
+import net.datastream.schemas.mp_results.mp3225_001.MP3225_AddEquipmentConfiguration_001_Result;
+import net.datastream.schemas.mp_results.mp3226_001.MP3226_SyncEquipmentConfiguration_001_Result;
 import net.datastream.wsdls.inforws.InforWebServicesPT;
 
-import javax.xml.ws.Holder;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class EquipmentConfigurationServiceImpl implements EquipmentConfigurationService {
 
@@ -30,31 +41,58 @@ public class EquipmentConfigurationServiceImpl implements EquipmentConfiguration
 
     @Override
     public String createEquipmentConfiguration(InforContext context, EquipmentConfigurationEntity equipmentConfiguration) throws InforException {
-        return null;
+        //Apply defaults
+        final EquipmentConfigurationDefault equipmentConfigurationDefault = readEquipmentDefaultConfiguration(context);
+        if (equipmentConfiguration.getRevisionNum() == null && equipmentConfigurationDefault.getREVISIONNUM() != null) {
+            equipmentConfiguration.setRevisionNum(equipmentConfigurationDefault.getREVISIONNUM().getVALUE());
+        }
+        if (equipmentConfiguration.getEquipmentConfigStatusCode() == null && equipmentConfigurationDefault.getEQUIPMENTCONFIGSTATUS() != null) {
+            equipmentConfiguration.setEquipmentConfigStatusCode(equipmentConfigurationDefault.getEQUIPMENTCONFIGSTATUS().getSTATUSCODE());
+        }
+        if (equipmentConfiguration.getEquipmentStatusCode() == null && equipmentConfigurationDefault.getSTATUS() != null) {
+            equipmentConfiguration.setEquipmentStatusCode(equipmentConfigurationDefault.getSTATUS().getSTATUSCODE());
+        }
+        if (equipmentConfiguration.getEquipmentTypeCode() == null && equipmentConfigurationDefault.getTYPE() != null) {
+            equipmentConfiguration.setEquipmentTypeCode(equipmentConfigurationDefault.getTYPE().getTYPECODE());
+        }
+        if (equipmentConfiguration.getOrganizationCode() == null && equipmentConfigurationDefault.getORGANIZATIONID() != null) {
+            equipmentConfiguration.setOrganizationCode(equipmentConfigurationDefault.getORGANIZATIONID().getORGANIZATIONCODE());
+        }
+        if (equipmentConfiguration.getAutoNumber() == null && equipmentConfigurationDefault.getAUTONUMBER() != null) {
+            equipmentConfiguration.setAutoNumber(DataTypeTools.decodeBoolean(equipmentConfigurationDefault.getAUTONUMBER()));
+        }
+
+        EquipmentConfiguration equipmentConfigurationFinal = tools.getInforFieldTools().transformWSHubObject(new EquipmentConfiguration(), equipmentConfiguration, context);
+
+        final MP3225_AddEquipmentConfiguration_001 mp3225_addEquipmentConfiguration_001 = new MP3225_AddEquipmentConfiguration_001();
+        mp3225_addEquipmentConfiguration_001.setEquipmentConfiguration(equipmentConfigurationFinal);
+
+        MP3225_AddEquipmentConfiguration_001_Result result = tools.performInforOperation(context, inforws::addEquipmentConfigurationOp, mp3225_addEquipmentConfiguration_001);
+        return result.getResultData().getEQUIPMENTCONFIGURATIONID().getEQUIPMENTCONFIGURATIONCODE();
     }
 
     @Override
     public String updateEquipmentConfiguration(InforContext context, EquipmentConfigurationEntity equipmentConfiguration) throws InforException {
-        return null;
+        final EquipmentConfiguration equipmentConfigurationOld = readInforEquipmentConfiguration(context, new EquipmentConfigurationId(equipmentConfiguration.getEquipmentConfigCode(), equipmentConfiguration.getRevisionNum()));
+        final EquipmentConfiguration equipmentConfigurationNew = tools.getInforFieldTools().transformWSHubObject(equipmentConfigurationOld, equipmentConfiguration, context);
+
+        final MP3226_SyncEquipmentConfiguration_001 syncEquipmentConfiguration = new MP3226_SyncEquipmentConfiguration_001();
+        syncEquipmentConfiguration.setEquipmentConfiguration(equipmentConfigurationNew);
+        MP3226_SyncEquipmentConfiguration_001_Result result = tools.performInforOperation(context, inforws::syncEquipmentConfigurationOp, syncEquipmentConfiguration);
+        return result.getResultData().getEQUIPMENTCONFIGURATIONID().getEQUIPMENTCONFIGURATIONCODE();
     }
 
-    private EquipmentConfiguration readInforEquipmentConfiguration(InforContext context, String equipmentConfigurationCode, BigDecimal revisionNum)
+    private EquipmentConfiguration readInforEquipmentConfiguration(InforContext context, EquipmentConfigurationId equipmentConfigurationId)
             throws InforException {
-
-        MP3228_GetEquipmentConfiguration_001 getEquipmentConfiguration = new MP3228_GetEquipmentConfiguration_001();
-        getEquipmentConfiguration.setEQUIPMENTCONFIGURATIONID(new EQUIPMENTCONFIGURATIONID_Type());
-        getEquipmentConfiguration.getEQUIPMENTCONFIGURATIONID().setORGANIZATIONID(tools.getOrganization(context));
-        getEquipmentConfiguration.getEQUIPMENTCONFIGURATIONID().setEQUIPMENTCONFIGURATIONCODE(equipmentConfigurationCode);
-        getEquipmentConfiguration.getEQUIPMENTCONFIGURATIONID().setREVISIONNUM(tools.getDataTypeTools().encodeQuantity(revisionNum, "Revision number"));
-
+        final MP3228_GetEquipmentConfiguration_001 mp3228_getEquipmentConfiguration_001 = tools.getInforFieldTools().transformWSHubObject(new MP3228_GetEquipmentConfiguration_001(), equipmentConfigurationId, context);
         MP3228_GetEquipmentConfiguration_001_Result getEquipmentConfigurationResult =
-            tools.performInforOperation(context, inforws::getEquipmentConfigurationOp, getEquipmentConfiguration);
+            tools.performInforOperation(context, inforws::getEquipmentConfigurationOp, mp3228_getEquipmentConfiguration_001);
         return getEquipmentConfigurationResult.getResultData().getEquipmentConfiguration();
     }
 
     @Override
-    public EquipmentConfigurationEntity readEquipmentConfiguration(InforContext context, String equipmentConfigurationCode, BigDecimal revisionNum) throws InforException {
-        EquipmentConfiguration inforEquipmentConfiguration = readInforEquipmentConfiguration(context, equipmentConfigurationCode, revisionNum);
+    public EquipmentConfigurationEntity readEquipmentConfiguration(InforContext context, EquipmentConfigurationId equipmentConfigurationId) throws InforException {
+        EquipmentConfiguration inforEquipmentConfiguration = readInforEquipmentConfiguration(context, equipmentConfigurationId);
         EquipmentConfigurationEntity equipmentConfiguration = tools.getInforFieldTools().transformInforObject(new EquipmentConfigurationEntity(), inforEquipmentConfiguration);
 
         tools.processRunnables(
@@ -67,12 +105,37 @@ public class EquipmentConfigurationServiceImpl implements EquipmentConfiguration
     }
 
     @Override
-    public EquipmentConfigurationEntity readEquipmentDefaultConfiguration(InforContext context, String equipmentConfigurationCode) throws InforException {
-        return null;
+    public EquipmentConfigurationDefault readEquipmentDefaultConfiguration(InforContext context) throws InforException {
+        MP3224_GetEquipmentConfigurationDefault_001 readEquipmentDefaultConfiguration = new MP3224_GetEquipmentConfigurationDefault_001();
+        readEquipmentDefaultConfiguration.setORGANIZATIONID(tools.getOrganization(context));
+        final MP3224_GetEquipmentConfigurationDefault_001_Result mp3224_getEquipmentConfigurationDefault_001_result = tools.performInforOperation(context, inforws::getEquipmentConfigurationDefaultOp, readEquipmentDefaultConfiguration);
+        return mp3224_getEquipmentConfigurationDefault_001_result.getResultData().getEquipmentConfigurationDefault();
     }
 
     @Override
-    public String deleteEquipmentConfiguration(InforContext context, String equipmentConfigurationCode) throws InforException {
-        return null;
+    public String deleteEquipmentConfiguration(InforContext context, EquipmentConfigurationId equipmentConfigurationId) throws InforException {
+        final MP3227_DeleteEquipmentConfiguration_001 deleteEquipmentConfigurationOp = tools.getInforFieldTools().transformWSHubObject(new MP3227_DeleteEquipmentConfiguration_001(), equipmentConfigurationId, context);
+        final MP3227_DeleteEquipmentConfiguration_001_Result mp3227_deleteEquipmentConfiguration_001_result = tools.performInforOperation(context, inforws::deleteEquipmentConfigurationOp, deleteEquipmentConfigurationOp);
+        return mp3227_deleteEquipmentConfiguration_001_result.getResultData().getEQUIPMENTCONFIGURATIONID().getEQUIPMENTCONFIGURATIONCODE();
+    }
+
+    @Override
+    public BatchResponse<String> createEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationEntity> equipmentConfigurationList) throws InforException {
+        return tools.batchOperation(context, this::createEquipmentConfiguration, equipmentConfigurationList);
+    }
+
+    @Override
+    public BatchResponse<EquipmentConfigurationEntity> readEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationId> equipmentConfigurationIdList) throws InforException {
+        return tools.batchOperation(context, this::readEquipmentConfiguration, equipmentConfigurationIdList);
+    }
+
+    @Override
+    public BatchResponse<String> updateEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationEntity> equipmentConfigurationList) throws InforException {
+        return tools.batchOperation(context, this::updateEquipmentConfiguration, equipmentConfigurationList);
+    }
+
+    @Override
+    public BatchResponse<String> deleteEquipmentConfigurationBatch(InforContext context, List<EquipmentConfigurationId> equipmentConfigurationIdList) throws InforException {
+        return tools.batchOperation(context, this::deleteEquipmentConfiguration, equipmentConfigurationIdList);
     }
 }
